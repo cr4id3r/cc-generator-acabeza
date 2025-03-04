009945835a0d22f7036d1813a4b89fdd0f38041a
acabeza <webs@paratyhotels.eu>
2025-03-04 13:41:17 +0100

    feat(manifest v3): migrated manifest.json to v3 and also upgraded some libraries

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,39 +1,26 @@
-import {Header} from "../components/header";
-import {Footer} from "../components/footer";
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
-// import {PageNotFound} from "./404";
-import {VisaComponent} from "./cards/visa";
-import {MasterCardComponent} from "./cards/mastercard";
-import {AXComponent} from "./cards/ax";
-
-
-// function WelcomeMessage() {
-//     return (
-//         <div>
-//              Bienvenido a CC Generator
-//         </div>
-//     )
-// }
-
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { VisaComponent } from "./cards/visa";
+import { MasterCardComponent } from "./cards/mastercard";
+import { AXComponent } from "./cards/ax";
 
 export default function CCGeneratorApp() {
-  return (
-    <div className={'app_wrapper'}>
-        <Router>
-            <Header/>
-
-            <div className="content_wrapper">
-                <Switch>
-                    <Route exact path="/" component={VisaComponent}/>
-                    <Route exact path="/visa" component={VisaComponent}/>
-                    <Route exact path="/mastercard" component={MasterCardComponent}/>
-                    <Route exact path="/ax" component={AXComponent}/>
-                    <Route component={VisaComponent}/>
-                </Switch>
-            </div>
-
-            <Footer/>
-        </Router>
-    </div>
-  );
+    return (
+        <div className="app_wrapper">
+            <Router>
+                <Header />
+                <div className="content_wrapper">
+                    <Routes>
+                        <Route path="/" element={<VisaComponent />} />
+                        <Route path="/visa" element={<VisaComponent />} />
+                        <Route path="/mastercard" element={<MasterCardComponent />} />
+                        <Route path="/ax" element={<AXComponent />} />
+                        <Route path="*" element={<VisaComponent />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
